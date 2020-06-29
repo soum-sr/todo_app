c32e873f5cbcdf8c775b6eb63fd1f0a0c9ba22fc
soum-sr <soum1071@gmail.com>
2020-06-29 23:20:27 +0530

    updated model

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+PRIORITY_CHOICES = (
+    ('High', 'HIGH'),
+    ('Mid', 'MIDIUM'),
+    ('Low', 'LOW')
+)
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=500)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=7, choices=PRIORITY_CHOICES, default='Low')
+    
