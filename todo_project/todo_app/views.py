05b248b4e6fef48ef5e3b5d0d7c3a32ad3581af6
soum-sr <soum1071@gmail.com>
2020-06-30 00:44:40 +0530

    added form and updated ui

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 # Create your views here.
@@ -8,5 +8,11 @@ def home(request):
     tasks = Task.objects.all()
     form = TaskForm()
 
+    if request.method == 'POST':
+        form  = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+        
     context = {'tasks':tasks,'form':form}
     return render(request, 'todo_app/home.html', context)
