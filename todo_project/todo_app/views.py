22510ba760a0f5379a0e892806f77d2e05480182
soum-sr <soum1071@gmail.com>
2020-06-30 00:18:16 +0530

    added form.py

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 # Create your views here.
 
 
 def home(request):
     tasks = Task.objects.all()
-    context = {'tasks':tasks}
+    form = TaskForm()
+
+    context = {'tasks':tasks,'form':form}
     return render(request, 'todo_app/home.html', context)
