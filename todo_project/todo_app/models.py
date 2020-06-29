2539d966aada5133e7fd659bd108f48093c45660
soum-sr <soum1071@gmail.com>
2020-06-29 23:25:24 +0530

    updated admin

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -11,4 +11,6 @@ class Task(models.Model):
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     priority = models.CharField(max_length=7, choices=PRIORITY_CHOICES, default='Low')
-    
+
+    def __str__(self):
+        return self.title
